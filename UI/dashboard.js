eba780f03a03c02fc01b2dec6783aad549cdf33d
harshkavdikar1 <harshkavdikar1@gmail.com>
2020-02-20 12:52:32 -0700

    Sending real time update to view with socket

--- UI/dashboard.js
+++ UI/dashboard.js
@@ -1,10 +1,30 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var kafka = require('kafka-node');
+var port = 8081;
+
 var Consumer = kafka.Consumer,
  client = new kafka.KafkaClient("localhost:9092"),
  consumer = new Consumer(
  client, [ { topic: 'processedtweets', partition: 0 } ], { autoCommit: false });
 
-consumer.on('message', function (message) {
-console.log(message);
+app.get('/', function(req, res){
+    res.sendfile('index.html');
 });
- 
+
+io = io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
+
+consumer = consumer.on('message', function(message) {
+    // console.log(message.value);
+    io.emit("message", message.value);
+});
+
+http.listen(port, function(){
+    console.log("Running on port " + port)
+});
